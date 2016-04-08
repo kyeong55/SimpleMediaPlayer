1320ec8ba3437ae69a560b20b9170f7c23ba2e9e
Taegyeong <kyeong102@gmail.com>
2016-04-09 01:55:32 +0900

    File List Activity test implemented

--- app/src/main/java/com/example/taegyeong/simplemediaplayer/FileListAdapter.java
+++ app/src/main/java/com/example/taegyeong/simplemediaplayer/FileListAdapter.java
@@ -6,12 +6,33 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by taegyeong on 16. 4. 8..
  */
+class FileInfo{
+    public String name;
+    public String path;
+    public FileInfo(String name, String path){
+        this.name = name;
+        this.path = path;
+    }
+}
 public class FileListAdapter extends  RecyclerView.Adapter<FileListAdapter.ViewHolder> {
+
+    private final String root = "/";
+
+    private List<FileInfo> items;
+    private TextView location;
+
+    public FileListAdapter(TextView location){
+        this.location = location;
+        getDir(root);
+    }
+
     @Override
     public FileListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.filelist_elem,parent,false);
@@ -20,23 +41,52 @@ public class FileListAdapter extends  RecyclerView.Adapter<FileListAdapter.ViewH
 
     @Override
     public void onBindViewHolder(FileListAdapter.ViewHolder holder, int position) {
+        final FileInfo item = items.get(position);
+        holder.name.setText(item.name);
+        holder.layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File file = new File(item.path);
+                if(file.isDirectory())
+                    getDir(item.path);
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
 
-    public void update(List<String>){
+    private void getDir(String dirPath) {
+        items = new ArrayList<>();
+        location.setText("Location: " + dirPath);
+        File f = new File(dirPath);
+        File[] files = f.listFiles();
+        if (!dirPath.equals(root)) {
+            items.add(new FileInfo(root,root));
+            items.add(new FileInfo("../",f.getParent()));
+        }
+
+        for (File file : files) {
+            if (file.isDirectory())
+                items.add(new FileInfo(file.getName() + "/",file.getPath()));
+            else
+                items.add(new FileInfo(file.getName(),file.getPath()));
+        }
+
+        this.notifyDataSetChanged();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView name;
+        View layout;
 
         public ViewHolder(View itemView) {
             super(itemView);
             name = (TextView)itemView.findViewById(R.id.filelist_elem_name);
+            layout = itemView;
         }
     }
 }
--- app/src/main/java/com/example/taegyeong/simplemediaplayer/MainActivity.java
+++ app/src/main/java/com/example/taegyeong/simplemediaplayer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.taegyeong.simplemediaplayer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -19,13 +20,16 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        final Intent fileListIntent = new Intent(this, FileListActivity.class);
+        fileListIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if (fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    startActivity(fileListIntent);
+                }
+            });
+        }
     }
 
     @Override
