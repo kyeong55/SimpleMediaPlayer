02fab5d29800be35a92c5020ee05d4dc406196b9
Taegyeong <kyeong102@gmail.com>
2016-04-23 03:03:22 +0900

    Select page created

--- app/src/main/java/com/example/taegyeong/simplemediaplayer/FilelistActivity.java
+++ app/src/main/java/com/example/taegyeong/simplemediaplayer/FilelistActivity.java
@@ -9,12 +9,14 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class FileListActivity extends AppCompatActivity {
 
     private View pageLoading;
+    private View pageSelect;
     private FileListAdapter fileListAdapter;
     private TextView fileListLocation;
 
@@ -26,16 +28,27 @@ public class FileListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_filelist);
 
         pageLoading = findViewById(R.id.main_loading_page);
+        pageSelect = findViewById(R.id.main_select_page);
+
         TextView loadingTitle1 = (TextView) findViewById(R.id.loading_title1);
         TextView loadingTitle2 = (TextView) findViewById(R.id.loading_title2);
         TextView loadingDetail = (TextView) findViewById(R.id.loading_detail);
 
+        Button selectImageButton = (Button) findViewById(R.id.select_image_button);
+        Button selectMusicButton = (Button) findViewById(R.id.select_music_button);
+        Button selectVideoButton = (Button) findViewById(R.id.select_video_button);
+
         TextView title = (TextView) findViewById(R.id.filelist_title);
         fileListLocation = (TextView) findViewById(R.id.filelist_location);
 
+        assert pageLoading != null;
+        assert pageSelect != null;
         assert loadingTitle1 != null;
         assert loadingTitle2 != null;
         assert loadingDetail != null;
+        assert selectImageButton != null;
+        assert selectMusicButton != null;
+        assert selectVideoButton != null;
         assert title != null;
         assert fileListLocation != null;
 
@@ -52,12 +65,38 @@ public class FileListActivity extends AppCompatActivity {
 
         ScanTask task = new ScanTask();
         task.execute();
+
+        selectImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectFileType(SMPCustom.TYPE_IMAGE);
+            }
+        });
+        selectMusicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectFileType(SMPCustom.TYPE_MUSIC);
+            }
+        });
+        selectVideoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectFileType(SMPCustom.TYPE_VIDEO);
+            }
+        });
     }
 
     @Override
     public void onBackPressed(){
-        if (!fileListAdapter.returnBack())
+        if (pageSelect.getVisibility()==View.VISIBLE)
             super.onBackPressed();
+        else if (!fileListAdapter.returnBack())
+            backToSelect();
+    }
+
+    public void backToSelect(){
+        pageSelect.setVisibility(View.VISIBLE);
+        fileListAdapter = null;
     }
 
     public void selectFileType(int fileType){
@@ -72,6 +111,8 @@ public class FileListActivity extends AppCompatActivity {
         fileListView.setLayoutManager(layoutManager);
         fileListView.setAdapter(fileListAdapter);
         fileListView.setVerticalScrollBarEnabled(true);
+
+        pageSelect.setVisibility(View.GONE);
     }
 
     public class ScanTask extends AsyncTask<Void, Void, Void> {
